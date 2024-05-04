f26bb89a00895705bf020ae095d55e7d21531a04
Rahul-Nikhate <rahulnikhate706636@gmail.com>
2024-05-04 14:53:24 +0530

    Database Connected

--- index.js
+++ index.js
@@ -2,6 +2,23 @@ const path = require('path');
 const express = require('express'); 
 const ejs = require("express");
 const app = express();
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    database: 'faculty_recruitment',
+    user: 'root',
+    password: 'Pass@123'
+});
+
+db.connect(function(error){
+    if(error){
+        throw error;
+    }
+    else{
+        console.log("Database connected");
+    }
+});
 
 app.use(express.static("public"));
 
